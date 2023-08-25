f9cccdebc5c26d528246c0068815ebf59c4bd888
Eric Weitz <eweitz@broadinstitute.org>
2023-08-25 16:21:51 -0400

    Refine contextual manual test steps

--- test/js/lib/cell-faceting.test.js
+++ test/js/lib/cell-faceting.test.js
@@ -7,8 +7,15 @@ import { initCellFaceting, filterCells } from 'lib/cell-faceting'
 describe('Cell faceting', () => {
   it('filters cells by group filters in annotation facets', async () => {
     // To manually test:
-    // 1. Go to DE pilot study ("Human milk" / "Cellular and transcriptional diversity over the course of human lactation")
-    // 2. (TODO) In "Annotation" menu, select "Category"
+    // 1. In ExploreDisplayTabs.jsx, uncomment the line `// window.SCP.updateFilteredCells = updateFilteredCells`
+    // 2. Go to DE pilot study ("Human milk - differential expression")
+    // 3. In the Console panel in DevTools, run `window.SCP.updateFilteredCells({'cell_type__ontology_label--group--study': ['epithelial cell']}) // Apply 1 facet, 1 filter`
+    // 4.  Confirm the cluster scatter plot updates to show only cells in the "LC1" and "LC2" groups, with cell counts 4427 and 35398 respectively.
+    // 5. In Console, run `window.SCP.updateFilteredCells({'cell_type__ontology_label--group--study': ['epithelial cell'], 'infant_sick_YN--group--study': ['yes', 'NA']}) // Apply 2 facets, 3 filters`
+    // 6. Confirm LC1 cell count updates to 1090, LC2 to 10725.
+    // 7. In Console, run `window.SCP.updateFilteredCells({}) // Clear filters`
+    // 8. Confirm cluster scatter plots returns to its original state
+    // 9. In ExploreDisplayTabs.jsx, comment out the line `// window.SCP.updateFilteredCells = updateFilteredCells`
 
     const fetchAnnotationFacets = jest.spyOn(ScpApi, 'fetchAnnotationFacets')
     // pass in a clone of the response since it may get modified by the cache operations
