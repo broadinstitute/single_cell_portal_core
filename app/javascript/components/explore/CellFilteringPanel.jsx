507f7baff77f7fe9b4a63ff3d91bfe3984bcc597
Eric Weitz <eweitz@broadinstitute.org>
2023-10-05 21:07:25 -0400

    Start implementing collapsible filters

--- app/javascript/components/explore/CellFilteringPanel.jsx
+++ app/javascript/components/explore/CellFilteringPanel.jsx
@@ -31,6 +31,16 @@ export function CellFilteringPanelHeader({
   )
 }
 
+/** Convert e.g. "cell_type__ontology_label" to "Cell type" */
+function getAnnotationDisplayName(annotationIdentifier) {
+  const rawName = annotationIdentifier.split('--')[0]
+  const sansOntologyName = rawName.replace('__ontology_label', '')
+  const sentenceCased = sansOntologyName[0].toUpperCase() + sansOntologyName.slice(1)
+  const spaced = sentenceCased.replace(/_/g, ' ')
+  const displayName = spaced
+  return displayName
+}
+
 /** Content for cell facet filter panel shown at right in Explore tab */
 export function CellFilteringPanel({
   annotationList,
@@ -57,44 +67,47 @@ export function CellFilteringPanel({
   const [options, setOptions] = useState()
 
   /** create the checklist for filtering the facet */
-  function createCellFilteringCheckList(singleCellFaceting) {
+  function CellFilterList({ facet }) {
+    console.log('facet', facet)
     // only create the checklist if the facet exists
-    if (Object.keys(singleCellFaceting).length !== 0
+    if (Object.keys(facet).length !== 0
     ) {
+      const facetName = getAnnotationDisplayName(facet.annotation)
       // grab the show facet names to filter the select options so there won't be duplicates
       const facetNames = shownFacets.map(facet => {return facet.annotation.split('--')[0]})
-      const otherMenuOptions = options.filter(opt => !facetNames.includes(opt.label))
-      return <div key={singleCellFaceting.annotation}>
-        <div>
-          <Select
-            name={singleCellFaceting.annotation}
-            options={otherMenuOptions}
-            data-analytics-name="annotation-select"
-            value={options.find(opt => opt.value.annotation === singleCellFaceting.annotation)}
-            onChange={(newAnnotation, event) => {
-              updateCheckedMap(newAnnotation, event)
-            }}
-          />
-        </div>
-        {singleCellFaceting.groups.map((item, index) => (
-          <div style={{ marginLeft: '5px' }} key={index}>
-            <label style={{ fontWeight: 'normal' }}>
-              <input
-                type="checkbox"
-                checked={isChecked(singleCellFaceting.annotation, item)}
-                value={item}
-                name={`${singleCellFaceting.annotation}:${item}`}
-                onChange={event => {
-                  handleCheck(event)
-                  updateFilteredCells(checkedMap)
-                }}
-                style={{ marginRight: '5px' }}
-              />
-              {item}
-            </label>
+      // const otherMenuOptions = options.filter(opt => !facetNames.includes(opt.label))
+
+      // Naturally sort groups (see https://en.wikipedia.org/wiki/Natural_sort_order)
+      const sortedGroups = facet.groups.sort((a, b) => {
+        return a[0].localeCompare(b[0], 'en', { numeric: true, ignorePunctuation: true })
+      })
+
+      return (
+        <div key={facet.annotation}>
+          <div style={{ fontWeight: 'bold', marginTop: '10px' }}>
+            {facetName}
           </div>
-        ))}
-      </div>
+          {sortedGroups.map((item, index) => (
+            <div style={{ marginLeft: '5px', lineHeight: '18px' }} key={index}>
+              <label className="cell-filter-label">
+                <input
+                  type="checkbox"
+                  checked={isChecked(facet.annotation, item)}
+                  value={item}
+                  data-analytics-name={`${facet.annotation}:${item}`}
+                  name={`${facet.annotation}:${item}`}
+                  onChange={event => {
+                    handleCheck(event)
+                    updateFilteredCells(checkedMap)
+                  }}
+                  style={{ marginRight: '5px' }}
+                />
+                {item}
+              </label>
+            </div>
+          ))}
+        </div>
+      )
     }
   }
 
@@ -102,8 +115,7 @@ export function CellFilteringPanel({
   function populateCheckedMap() {
     const tempCheckedMap = {}
 
-    // only initalize up to three facets for now
-    const numFacets = initialFiveFacets.length > 2 ? 3 : initialFiveFacets.length
+    const numFacets = initialFiveFacets.length
     for (let i = 0; i < numFacets; i++) {
       tempCheckedMap[initialFiveFacets[i].annotation] = initialFiveFacets[i].groups
     }
@@ -143,7 +155,6 @@ export function CellFilteringPanel({
     // set the facets that are shown in the UI
     setShownFacets(tempShownFacets)
   }
-
 
   /** Add/Remove checked item from list */
   function handleCheck(event) {
@@ -209,18 +220,22 @@ export function CellFilteringPanel({
             styles={clusterSelectStyle}/>
         </label>
         { Object.keys(checkedMap).length !== 0 &&
-        <div style={{ 'marginTop': '5px' }}>
-          <h5>Filter by&nbsp;
+        <div style={{ marginTop: '10px', height: '450px', overflowY: 'scroll' }}>
+          <b>Filter &nbsp;
             <a className="action help-icon"
               data-toggle="tooltip"
               data-original-title="Use the checkboxes to filter points from the plot.  Deselected values are
                 assigned to the '--Filtered--' group. Hover over this legend entry to highlight."
             >
               <FontAwesomeIcon icon={faInfoCircle}/>
-            </a></h5>
+            </a></b>
           <div style={{ margin: '2px', padding: '2px' }}>
-            { shownFacets.map(singleFacet => {
-              return createCellFilteringCheckList(singleFacet)
+            { shownFacets.map(facet => {
+              return (
+                <CellFilterList
+                  facet={facet}
+                />
+              )
             })}
           </div>
         </div>}
