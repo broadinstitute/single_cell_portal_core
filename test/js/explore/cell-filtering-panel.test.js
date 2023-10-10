aff1fd824f518a69cb0f353f6e4f4df520452116
Eric Weitz <eweitz@broadinstitute.org>
2023-10-10 13:08:57 -0400

    Test all-facets toggle

--- test/js/explore/cell-filtering-panel.test.js
+++ test/js/explore/cell-filtering-panel.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen, waitFor } from '@testing-library/react'
+import { render, fireEvent, screen, waitFor } from '@testing-library/react'
 import { CellFilteringPanel } from '~/components/explore/CellFilteringPanel'
 import {
   annotationList,
@@ -34,8 +34,45 @@ describe('"Cell filtering" panel', () => {
 
     // screen.debug(container) // Print cell filtering panel HTML
 
-    const header = container.querySelector('h5')
+    const header = container.querySelector('.filter-section-header')
 
     expect(header).toHaveTextContent('Filter by')
+  })
+
+  it('renders initially', async () => {
+    const cluster = 'All Cells UMAP'
+    const shownAnnotation = {
+      'name': 'General_Celltype',
+      'type': 'group',
+      'scope': 'study',
+      'isDisabled': false
+    }
+
+    // Mock functions
+    const updateClusterParams = jest.fn()
+    const updateFilteredCells = jest.fn()
+
+    const { container } = render(
+      <CellFilteringPanel
+        annotationList={annotationList}
+        cluster={cluster}
+        shownAnnotation={shownAnnotation}
+        updateClusterParams={updateClusterParams}
+        cellFaceting={cellFaceting}
+        updateFilteredCells={updateFilteredCells}
+      />
+    )
+
+    // screen.debug(container) // Print cell filtering panel HTML
+
+    const allFacetsToggle = container.querySelector('.filter-section-header .facet-toggle-chevron')
+
+    fireEvent.click(allFacetsToggle)
+
+    const facetHeaders = document.querySelectorAll('.cell-facet-header')
+    const isAllFacetsHidden = Array.from(facetHeaders).every(
+      facetNode => Array.from(facetNode.classList).includes('cell-filters-hidden')
+    )
+    expect(isAllFacetsHidden).toEqual(true)
   })
 })
