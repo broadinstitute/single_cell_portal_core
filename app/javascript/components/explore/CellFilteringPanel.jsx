d4efe8710a1582cf532dd96e1e7796b8674edb6d
Eric Weitz <eweitz@broadinstitute.org>
2023-11-15 12:23:20 -0500

    Replace end-bulb with soft green for full count

--- app/javascript/components/explore/CellFilteringPanel.jsx
+++ app/javascript/components/explore/CellFilteringPanel.jsx
@@ -212,13 +212,12 @@ function BaselineSparkbar({ facet, filter }) {
   const maxWidthPx = `${maxWidth}px`
   const filteredWidthPx = `${filteredWidth}px`
 
+  const fullClass = baselineCount === filteredCount ? ' full' : ''
+
   return (
     <>
       <span className="sparkbar">
-        {baselineCount === filteredCount &&
-          <span className="sparkbar-bulb sparkbar-filtered" style={{ height: '4px', width: '1.25px', right: maxWidthPx, top: '-1px', backgroundColor: '#7CF' }}></span>
-        }
-        <span className="sparkbar-filtered" style={{ width: filteredWidthPx }}> </span>
+        <span className={`sparkbar-filtered ${fullClass}`} style={{ width: filteredWidthPx }}> </span>
         <span className="sparkbar-baseline" style={{ width: maxWidthPx, left: filteredWidthPx }}></span>
       </span>
     </>
