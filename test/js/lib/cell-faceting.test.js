e7ada1f617364bebc0556032619143baa893723d
Eric Weitz <eweitz@broadinstitute.org>
2024-03-25 13:27:44 -0400

    Test fix for filtering when numeric annotation has constant value

--- test/js/lib/cell-faceting.test.js
+++ test/js/lib/cell-faceting.test.js
@@ -68,4 +68,117 @@ describe('Cell faceting', () => {
     expect(applyNumericFilters(10, [[['between', [0, 2]], ['between', [8, 20]]], true])).toStrictEqual(true)
     expect(applyNumericFilters(5, [[['between', [0, 2]], ['between', [8, 20]]], true])).toStrictEqual(false)
   })
+
+  it('filters cells even when a raw data has numeric annotation with 1 value for all cells', async () => {
+    // This tests a fix for a bug in cell filtering that was first observed in production SCP2126.
+    // The bug causes cell filtering to fail when any filter is changed.  It is caused by
+    // a numeric facet ("Weeks post dose") that has the same number (4) for all cells.  Cell filtering
+    // requires a set of cells to have > 1 value.  This tests special handling when that constraint is
+    // violated in numeric annotations.
+    //
+    // Once ticket SCP-5572 is resolved, this test will be obsolete and should be deleted.
+
+    const allAnnotsScp5554 = [
+      {
+        'name': 'sex',
+        'type': 'group',
+        'values': [
+          'female',
+          'male'
+        ],
+        'scope': 'study',
+        'is_differential_expression_enabled': false
+      },
+      {
+        'name': 'cell_type__ontology_label',
+        'type': 'group',
+        'values': [
+          'neuron',
+          'fibroblast'
+        ],
+        'scope': 'study',
+        'is_differential_expression_enabled': false
+      },
+      {
+        'name': 'weeks_post_dose',
+        'type': 'numeric',
+        'values': [],
+        'scope': 'study',
+        'is_differential_expression_enabled': false
+      }
+    ]
+
+    const constantNumericValue = 4
+    const facetDataScp5554 = {
+      'cells': [
+        [1, 1, constantNumericValue],
+        [0, 1, constantNumericValue],
+        [1, 0, constantNumericValue],
+        [0, 0, constantNumericValue]
+      ],
+      'facets': [
+        {
+          'annotation': 'cell_type__ontology_label--group--study',
+          'groups': [
+            'neuron',
+            'fibroblast'
+          ]
+        },
+        {
+          'annotation': 'sex--group--study',
+          'groups': [
+            'female',
+            'male'
+          ]
+        },
+        {
+          'annotation': 'weeks_post_dose--numeric--study',
+          'groups': []
+        }
+      ]
+    }
+
+
+    const fetchAnnotationFacets = jest.spyOn(ScpApi, 'fetchAnnotationFacets')
+    // pass in a clone of the response since it may get modified by the cache operations
+    fetchAnnotationFacets.mockImplementation(() => Promise.resolve(
+      facetDataScp5554
+    ))
+
+    // Test client-side cell faceting setup functionality
+    const selectedCluster = 'scp_clustering_mouse_thalamus.tsv'
+    const selectedAnnot = { name: 'Category', type: 'group', scope: 'study' }
+    const studyAccession = 'SCP174'
+
+    const cellFaceting = await initCellFaceting(
+      selectedCluster, selectedAnnot, studyAccession, allAnnotsScp5554
+    )
+
+    const cellsByFacet = cellFaceting.cellsByFacet
+    const facets = cellFaceting.facets
+    const filterableCells = cellFaceting.filterableCells
+
+    const expectedFilterableCells3 = {
+      'allCellsIndex': 3,
+      'facetIndex': [0, 0, 4]
+    }
+
+    expect(filterableCells[3]).toMatchObject(expectedFilterableCells3)
+
+    // Test actual cell faceting
+    const selections = {
+      'cell_type__ontology_label--group--study': ['neuron'],
+
+      // The `undefined` value below is how problematic facets -- i.e. numeric
+      // annotations with 1 value for all cells -- get passed in to `filterCells`.
+      // https://github.com/broadinstitute/single_cell_portal_core/pull/1996
+      'weeks_post_dose--numeric--study': undefined
+    }
+    const filterCellsResult = filterCells(
+      selections, cellsByFacet, facets, filterableCells, facets
+    )
+    console.log('filterCellsResult', filterCellsResult)
+    // All cells in this example are neurons,
+    expect(filterCellsResult[0]).toHaveLength(2)
+  })
 })
