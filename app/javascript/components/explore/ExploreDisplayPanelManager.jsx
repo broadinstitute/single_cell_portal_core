20e8916ef2b712ec6d26e2b0563d1ad1bb44c3b6
Eric Weitz <eweitz@broadinstitute.org>
2024-06-01 09:25:23 -0400

    Show view options for "Genome" tab

--- app/javascript/components/explore/ExploreDisplayPanelManager.jsx
+++ app/javascript/components/explore/ExploreDisplayPanelManager.jsx
@@ -234,7 +234,7 @@ export default function ExploreDisplayPanelManager({
 
   const annotationList = exploreInfo ? exploreInfo.annotationList : null
   // hide the cluster controls if we're on a genome tab, or if there aren't clusters to choose
-  const showClusterControls = !['genome', 'infercnv-genome', 'geneListHeatmap'].includes(shownTab) &&
+  const showClusterControls = !['infercnv-genome', 'geneListHeatmap'].includes(shownTab) &&
                                 annotationList?.clusters?.length
 
   const shownAnnotation = getShownAnnotation(exploreParamsWithDefaults.annotation, annotationList)
@@ -506,14 +506,14 @@ export default function ExploreDisplayPanelManager({
             <div id={useBookmarkContainer ? 'bookmark-container' : ''} className={useRowClass ? 'row' : ''}>
               <div className={useRowClass ? 'col-xs-12' : ''}>
                 <button className="action action-with-bg"
-                        onClick={clearExploreParams}
-                        title="Reset all view options"
-                        data-analytics-name="explore-view-options-reset">
+                  onClick={clearExploreParams}
+                  title="Reset all view options"
+                  data-analytics-name="explore-view-options-reset">
                   <FontAwesomeIcon icon={faUndo}/> Reset view</button>
                 <button onClick={copyLink}
-                        className="action action-with-bg"
-                        data-toggle="tooltip"
-                        title="Copy a link to this visualization to the clipboard">
+                  className="action action-with-bg"
+                  data-toggle="tooltip"
+                  title="Copy a link to this visualization to the clipboard">
                   <FontAwesomeIcon icon={faLink}/> Get link</button>
                 {exploreInfo?.bookmarks &&
                   <BookmarkManager
