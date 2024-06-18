b49cb5fe65cb01c2ff6f377c756972a1d48be6d8
Eric Weitz <eweitz@broadinstitute.org>
2024-06-18 09:40:18 -0400

    Further refine warning style and conditions

--- app/javascript/components/visualization/controls/SpatialSelector.jsx
+++ app/javascript/components/visualization/controls/SpatialSelector.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Select from '~/lib/InstrumentedSelect'
 import { clusterSelectStyle } from '~/lib/cluster-utils'
@@ -9,10 +9,14 @@ function getSpatialOptions(allSpatialGroups) {
   return clusterList.map(group => {return { label: group.name, value: group.name }})
 }
 
-/**  */
-function InlineClearButton() {
+/** Small "x" button to clear a message */
+function InlineClearButton({ setShowMessage, issueType }) {
   return (
-    <button className="clear-warning-inline">
+    <button
+      className={`clear-${issueType}-inline`}
+      onClick={() => setShowMessage(false)}
+      title="Dismiss message"
+    >
       <svg height="14" width="14" viewBox="0 0 20 20">
         <path d="M14.348 14.849c-0.469 0.469-1.229 0.469-1.697 0l-2.651-3.030-2.651 3.029c-0.469 0.469-1.229 0.469-1.697 0-0.469-0.469-0.469-1.229 0-1.697l2.758-3.15-2.759-3.152c-0.469-0.469-0.469-1.228 0-1.697s1.228-0.469 1.697 0l2.652 3.031 2.651-3.031c0.469-0.469 1.228-0.469 1.697 0s0.469 1.229 0 1.697l-2.758 3.152 2.758 3.15c0.469 0.469 0.469 1.229 0 1.698z">
         </path>
@@ -35,7 +39,29 @@ function getShouldWarn(spatialGroups, genes) {
   return (
     (numGenes === 0 && numSpatial > 5) ||
     (numGenes === 1 && numSpatial > 2) ||
-    (numGenes > 2 && numSpatial > 5)
+    (numGenes > 1 && numSpatial > 5)
+  )
+}
+
+/** Small, contextual message and clear-message button */
+function InlineMessage({ text, issueType, genes }) {
+  const [showMessage, setShowMessage] = useState(true)
+
+  useEffect(() => {
+    setShowMessage(true)
+  }, [genes.join()])
+
+  if (!showMessage) {
+    return <></>
+  }
+
+  return (
+    <span className={`${issueType}-inline`}>{text}
+      <InlineClearButton
+        setShowMessage={setShowMessage}
+        issueType={issueType}
+      />
+    </span>
   )
 }
 
@@ -47,6 +73,9 @@ function getShouldWarn(spatialGroups, genes) {
 export default function SpatialSelector({ spatialGroups, updateSpatialGroups, allSpatialGroups, genes }) {
   const options = getSpatialOptions(allSpatialGroups)
   const shouldWarn = getShouldWarn(spatialGroups, genes)
+
+  console.log('shouldWarn', shouldWarn)
+
   return (
     <div className="form-group">
       <label className="labeled-select">Spatial groups
@@ -61,7 +90,11 @@ export default function SpatialSelector({ spatialGroups, updateSpatialGroups, al
           styles={clusterSelectStyle}/>
       </label>
       {shouldWarn &&
-        <span className="warning-inline">Remove groups to avoid plot limit.<InlineClearButton/></span>
+        <InlineMessage
+          text="Remove groups to avoid plot limit."
+          issueType="warning"
+          genes={genes}
+        />
       }
     </div>
   )
