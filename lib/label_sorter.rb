fb0ff55617f8b0a43d14dc284fe56cdbfb1dd29a
bistline <bistline@broadinstitute.org>
2024-07-23 13:53:25 -0400

    Safeguard against nil values in LabelSorter, move --Unspecified-- to end of color map

--- lib/label_sorter.rb
+++ lib/label_sorter.rb
@@ -5,8 +5,8 @@ class LabelSorter
   attr_reader :lowercase, :natural_types, :type_order
 
   def initialize(str)
-    @str = str
-    @lowercase = str.downcase
+    @str = str.to_s # safeguard against nil or non-string values
+    @lowercase = @str.downcase
     @natural_types = @lowercase.scan(/\d+|\D+/).map { |s| s =~ /\d/ ? s.to_i : s }
     @type_order = @natural_types.map { |el| el.is_a?(Integer) ? :i : :s }.join
   end
@@ -24,6 +24,11 @@ class LabelSorter
   end
 
   def self.natural_sort(values)
-    values.map { |v| new(v) }.sort.map { |el| el.to_s }
+    sorted = values.map { |v| new(v) }.sort.map { |el| el.to_s }
+    # move any blank/Unspecified entries to the end to allow use of first color for actual label
+    if sorted.first.blank? || sorted.first == AnnotationVizService::MISSING_VALUE_LABEL
+      sorted << sorted.shift
+    end
+    sorted
   end
 end
