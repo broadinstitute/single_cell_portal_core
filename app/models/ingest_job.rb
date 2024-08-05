20945806c56f9deaa2201719990201d42aaa9ea7
bistline <bistline@broadinstitute.org>
2024-08-05 15:04:26 -0400

    Enable subsampling on AnnData, fixing DataArray validation, exp cells query

--- app/models/ingest_job.rb
+++ app/models/ingest_job.rb
@@ -594,7 +594,7 @@ class IngestJob
       end
     when 'AnnData'
       # TODO (SCP-5140): subsampling logic works but MongoDB throws index uniqueness violations due to same study_file_id
-      return if study_file.is_anndata?
+      # return if study_file.is_anndata?
 
       file_info = study_file.ann_data_file_info
       if file_info.has_clusters? && file_info.has_metadata?
--- app/models/study.rb
+++ app/models/study.rb
@@ -1413,8 +1413,9 @@ class Study
 
   # return the cells found in a single expression matrix
   def expression_matrix_cells(study_file)
+    filename = study_file.is_viz_anndata? ? 'h5ad_frag.matrix.processed.mtx.gz' : study_file.upload_file_name
     query = {
-      name: "#{study_file.upload_file_name} Cells", array_type: 'cells', linear_data_type: 'Study',
+      name: "#{filename} Cells", cluster_name: filename, array_type: 'cells', linear_data_type: 'Study',
       linear_data_id: self.id, study_file_id: study_file.id, cluster_group_id: nil, subsample_annotation: nil,
       subsample_threshold: nil
     }
