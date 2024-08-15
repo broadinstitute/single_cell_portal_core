537a9fe42bbd5b5a837a983d8de0d4dbeb75310f
Eric Weitz <eweitz@broadinstitute.org>
2024-08-15 14:35:41 -0400

    Add negative test data AnnData, stub test

--- test/js/lib/validate-anndata.test.js
+++ test/js/lib/validate-anndata.test.js
@@ -1,8 +1,18 @@
 import {parseAnnDataFile} from 'lib/validation/validate-anndata'
 
 describe('Client-side file validation for AnnData', () => {
-  it('Validates AnnData file headers', async () => {
-    const url = 'https://github.com/broadinstitute/scp-ingest-pipeline/raw/development/tests/data/anndata/anndata_test.h5ad'
-    const parseResults = parseAnnDataFile(url)
+  it('Reports SCP-valid AnnData file as valid', async () => {
+    // eslint-disable-next-line max-len
+    const url = 'https://github.com/broadinstitute/single_cell_portal_core/raw/development/test/test_data/anndata_test.h5ad'
+    const parseResults = await parseAnnDataFile(url)
+    expect(parseResults.issues).toHaveLength(0)
   })
+
+  // TODO:
+  // it('Reports SCP-invalid AnnData file as invalid', async () => {
+  //   // eslint-disable-next-line max-len
+  //   const url = 'https://github.com/broadinstitute/single_cell_portal_core/raw/development/test/test_data/anndata/anndata_test_bad_header_no_species.h5ad'
+  //   const parseResults = await parseAnnDataFile(url)
+  // expect(parseResults.issues.length).toBeGreaterThan(0)
+  // })
 })
