41a3072af2c4f0de9b0f70b7ce38579527f8afef
Eric Weitz <eweitz@broadinstitute.org>
2024-08-21 08:27:19 -0400

    Add test data, document how to reproduce; scaffold new test

--- test/js/lib/pathway-expression.test.js
+++ test/js/lib/pathway-expression.test.js
@@ -1,4 +1,4 @@
-import { getPathwayGenes } from '~/lib/pathway-expression'
+import { getPathwayGenes, colorPathwayGenesByExpression } from '~/lib/pathway-expression'
 
 import { pathwayContainerHtml } from './pathway-expression.test-data.js'
 
@@ -14,4 +14,15 @@ describe('Expression overlay for pathway diagram', () => {
     expect(pathwayGenes).toHaveLength(4)
     expect(pathwayGenes[0]).toEqual({ domId: 'e8c90', name: 'CSN2' })
   })
+
+  it('colors pathway genes by expression', () => {
+    const body = document.querySelector('body')
+    body.insertAdjacentHTML('beforeend', pathwayContainerHtml)
+    const ranks = ['CSN2', 'NR3C1', 'EGFR', 'PRL']
+    const pathwayGenes = getPathwayGenes(ranks)
+    // const dotPlotMetrics =
+    colorPathwayGenesByExpression(genes, dotPlotMetrics, annotationLabel)
+  })
+
+
 })
