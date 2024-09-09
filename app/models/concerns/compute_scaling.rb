522a8453f26f229730693194d9052c4cda9140f8
bistline <bistline@broadinstitute.org>
2024-09-09 15:55:04 -0400

    adding guard clauses for defined consts/params

--- app/models/concerns/compute_scaling.rb
+++ app/models/concerns/compute_scaling.rb
@@ -6,8 +6,9 @@ module ComputeScaling
   # produces a hash with entries like { 'n2-highmem-4' => 0..16.gigabytes }
   # change GB_PER_CORE in parent class to adjust scaling (1-8, lower numbers means more aggressive scaling)
   def scaled_machine_types
+    gb_per_core = defined?(self.class::GB_PER_CORE) && self.class::GB_PER_CORE || 4
     num_cores = [4, 8, 16, 32, 48, 64]
-    ram_per_core = num_cores.map { |core| (core * self.class::GB_PER_CORE).gigabytes }.freeze
+    ram_per_core = num_cores.map { |core| (core * gb_per_core).gigabytes }.freeze
     num_cores.map.with_index do |cores, index|
       floor = index == 0 ? 0 : ram_per_core[index - 1]
       limit = index == num_cores.count - 1 ? ram_per_core[index] * 2 : ram_per_core[index]
@@ -18,11 +19,13 @@ module ComputeScaling
 
   # default machine_type for parameters class
   def default_machine_type
-    self.class::PARAM_DEFAULTS[:machine_type] || scaled_machine_types.keys.first
+    defined?(self.class::PARAM_DEFAULTS) && self.class::PARAM_DEFAULTS[:machine_type] || scaled_machine_types.keys.first
   end
 
   # set machine_type based on file_size, using class defaults if specified
   def assign_machine_type
+    return default_machine_type if file_size.blank?
+
     max_file_size = scaled_machine_types.values.last.last
     return scaled_machine_types.keys.last if file_size > max_file_size
 
