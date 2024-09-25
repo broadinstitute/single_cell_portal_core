c12b3756b4d098c2f70c50b688358dcf1430fc7a
Eric Weitz <eweitz@broadinstitute.org>
2024-09-25 16:32:46 -0400

    Finish alphanumeric + underscore validation, add test data

--- app/javascript/lib/validation/validate-anndata.js
+++ app/javascript/lib/validation/validate-anndata.js
@@ -3,6 +3,7 @@ import { openH5File } from 'hdf5-indexed-reader'
 import { getOAuthToken } from '~/lib/scp-api'
 import {
   validateUnique, validateRequiredMetadataColumns,
+  validateAlphanumericAndUnderscores,
   metadataSchema, REQUIRED_CONVENTION_COLUMNS
 } from './shared-validation'
 import { getAcceptedOntologies, fetchOntologies } from './ontology-validation'
@@ -285,6 +286,7 @@ export async function parseAnnDataFile(fileOrUrl, remoteProps) {
 
   issues = issues.concat(
     validateUnique(headers),
+    validateAlphanumericAndUnderscores(headers),
     requiredMetadataIssues,
     ontologyIdFormatIssues,
     ontologyLabelAndIdIssues
