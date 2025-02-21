123362bd5216451db51cd61af6573e10ff36d119
Eric Weitz <eweitz@broadinstitute.org>
2025-02-21 15:16:33 -0500

    Extract validateEmbargo to JS module

--- app/javascript/vite/application.js
+++ app/javascript/vite/application.js
@@ -15,6 +15,7 @@ import ClusterAssociationSelect from '~/components/upload/ClusterAssociationSele
 import RawAssociationSelect from '~/components/upload/RawAssociationSelect'
 import { getFeatureFlagsWithDefaults } from '~/providers/UserProvider'
 import ValidateFile from '~/lib/validation/validate-file'
+import { validateEmbargo } from '~/lib/validation/validate-study-creation'
 import { setupSentry } from '~/lib/sentry-logging'
 import { adjustGlobalHeader, mitigateStudyOverviewTitleTruncation } from '~/lib/layout-utils'
 import { clearOldServiceWorkerCaches } from '~/lib/service-worker-cache'
@@ -132,6 +133,7 @@ window.SCP.eventsToLog.forEach(eventToLog => {
 
 window.SCP.getFeatureFlagsWithDefaults = getFeatureFlagsWithDefaults
 window.SCP.validateRemoteFile = validateRemoteFile
+window.SCP.validateEmbargo = validateEmbargo
 window.SCP.API = ScpApi
 
 window.Spinner = Spinner
