dca1a73062007144933d93093f2d96bbf8ad22d4
bistline <bistline@broadinstitute.org>
2025-03-20 17:01:46 -0400

    trying changing ubuntu uid to 9876

--- bin/deploy-github.sh
+++ bin/deploy-github.sh
@@ -12,7 +12,7 @@ THIS_DIR="$(cd "$(dirname -- "$0")"; pwd)"
 
 function main {
   # defaults
-  SSH_USER="ubuntu"
+  SSH_USER="jenkins"
   DESTINATION_BASE_DIR='/home/jenkins/deployments/single_cell_portal_core'
   GIT_BRANCH="main"
   PASSENGER_APP_ENV="production"
--- set_user_permissions.bash
+++ set_user_permissions.bash
@@ -2,7 +2,9 @@
 if [[ $PASSENGER_APP_ENV = "production" ]] || [[ $PASSENGER_APP_ENV = "staging" ]] || [[ $PASSENGER_APP_ENV = "pentest" ]]
 then
 	set -e # fail on any error
-
+  # gotcha for issue with ubuntu user clashing with app user id
+  echo '*** CHANGING ubuntu UID to 9876 ***'
+  usermod -o -u 9876 ubuntu || true
 	echo '*** SETTING UID AND GID TO MATCH HOST VOLUMES ***'
 	TARGET_UID=$(stat -c "%u" /home/app/webapp)
 	echo '-- Setting app user to use uid '$TARGET_UID
