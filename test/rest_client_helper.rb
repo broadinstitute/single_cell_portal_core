c8cf378dbf9916b4aa7c1f89068b03e327612cc9
bistline <bistline@broadinstitute.org>
2025-03-24 14:35:00 -0400

    properly overwrite method via module_eval

--- test/rest_client_helper.rb
+++ test/rest_client_helper.rb
@@ -1,13 +1,10 @@
-# patch to handle issues of inconsistency
-module RestClient
-  class Exception < RuntimeError
-    def http_code
-      # return integer for compatibility
-      if @response
-        @response.try(:code)&.to_i || @response.try(:http_code)&.to_i || @response
-      else
-        @initial_response_code
-      end
+require 'rest_client'
+RestClient::Exception.module_eval do
+  def http_code
+    if @response
+      @response.try(:code)&.to_i || @response.try(:http_code)&.to_i || @response
+    else
+      @initial_response_code
     end
   end
 end
