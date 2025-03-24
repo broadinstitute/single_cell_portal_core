15698aaa99980c444f2ae94e3a233d0a6a45cfc3
bistline <bistline@broadinstitute.org>
2025-03-24 11:54:19 -0400

    reverting user change

--- bin/remount_portal_source.sh
+++ bin/remount_portal_source.sh
@@ -9,8 +9,8 @@
 # More context: https://github.com/broadinstitute/single_cell_portal_core/pull/2216
 MOUNT_DIR=$(ls -l /dev/disk/by-id/google-* | grep google-singlecell-data-disk | awk -F '/' '{ print $NF }')
 if [[ -n "$MOUNT_DIR" ]]; then
-  echo "$(date): remounting google-singlecell-data-disk from /dev/$MOUNT_DIR" >> /home/ubuntu/remount_log.txt
-  mount -o discard,defaults /dev/$MOUNT_DIR /home/ubuntu/deployments
+  echo "$(date): remounting google-singlecell-data-disk from /dev/$MOUNT_DIR" >> /home/jenkins/remount_log.txt
+  mount -o discard,defaults /dev/$MOUNT_DIR /home/jenkins/deployments
 else
   echo -e "$(date): cannot remount google-singlecell-data-disk, available disks:\n$(ls -l /dev/disk/by-id/google-*)" >> /home/ubuntu/remount_log.txt
 fi
