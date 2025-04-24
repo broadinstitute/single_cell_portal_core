7dfe350d5958c12dd8c23acf27d90449afd99325
Eric Weitz <eweitz@broadinstitute.org>
2025-04-24 10:10:11 -0400

    Test study gene search responds to input, shows gene matches

--- test/js/explore/study-gene-keyword.test.js
+++ test/js/explore/study-gene-keyword.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
-import { render } from '@testing-library/react'
+import { render, waitFor, screen, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 import StudyGeneField from 'components/explore/StudyGeneField'
 
 
@@ -28,6 +29,25 @@ describe('Search query display text', () => {
 
     const { container: loadingContainer } = render(<StudyGeneField queries={[]} queryFn={() => {}} allGenes={[]} speciesList={[]} isLoading={true}/>)
     expect(loadingContainer.querySelector('.gene-keyword-search-input').textContent.trim()).toEqual('Search gene(s) and find plots')
+  })
+
+  it('responds to text input and shows matching gene options', async () => {
+    const { container } = render(
+      <StudyGeneField queries={[]} queryFn={() => {}} allGenes={['PTEN']} speciesList={[]} />
+    )
+
+    // Find the input field inside react-select
+    const input = container.querySelector('[role="combobox"]')
+
+    fireEvent.change(input, { target: { value: 'PT' } })
+
+    expect(input).toHaveValue('PT')
+
+    // Wait for dropdown to show
+    await waitFor(() => {
+      const ptenElement = screen.getByText(/PTEN/)
+      expect(ptenElement).toBeInTheDocument()
+    })
   })
 })
 
