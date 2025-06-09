b1ee7f9506e9060127d32719bcccd28bbc976557
bistline <bistline@broadinstitute.org>
2025-06-09 14:30:04 -0400

    adding missing migration

---
+++ db/migrate/20250609182851_make_all_facets_mongo_based.rb
@@ -0,0 +1,9 @@
+class MakeAllFacetsMongoBased < Mongoid::Migration
+  def self.up
+    SearchFacet.update_all(is_mongo_based: true)
+  end
+
+  def self.down
+    SearchFacet.where(is_presence_facet: false).update_all(is_mongo_based: true)
+  end
+end
