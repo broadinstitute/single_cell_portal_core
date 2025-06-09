eb1bec2ddcf01d9741bca8557113fb23941aadd5
bistline <bistline@broadinstitute.org>
2025-06-09 14:45:28 -0400

    add missing minmax setter to migration

--- db/migrate/20250609182851_make_all_facets_mongo_based.rb
+++ db/migrate/20250609182851_make_all_facets_mongo_based.rb
@@ -1,6 +1,7 @@
 class MakeAllFacetsMongoBased < Mongoid::Migration
   def self.up
     SearchFacet.update_all(is_mongo_based: true)
+    CellMetadatum.where(name: 'organism_age').map(&:set_minmax_by_units!)
   end
 
   def self.down
