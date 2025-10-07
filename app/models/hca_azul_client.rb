70c95c3441a5c92b07ed35b488b74baf88899453
bistline <bistline@broadinstitute.org>
2025-10-07 11:50:42 -0400

    lowering HcaAzulClient::MAX_RESILTS re: 502 errors

--- app/models/hca_azul_client.rb
+++ app/models/hca_azul_client.rb
@@ -15,7 +15,7 @@ class HcaAzulClient
   MANIFEST_FORMATS = %w[compact full terra.bdbag terra.pfb curl].freeze
 
   # maximum number of results to return
-  MAX_RESULTS = 100
+  MAX_RESULTS = 80
 
   # maximum length of query string (in characters) for requests
   MAX_QUERY_LENGTH = 8192
