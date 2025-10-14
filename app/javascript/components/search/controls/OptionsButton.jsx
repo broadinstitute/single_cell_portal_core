8e790aaeaef0d227cf697aaf773d42ddbe506aa7
bistline <bistline@broadinstitute.org>
2025-10-14 12:28:48 -0400

    adding clear button for options only, test updates

--- app/javascript/components/search/controls/OptionsButton.jsx
+++ app/javascript/components/search/controls/OptionsButton.jsx
@@ -1,22 +1,45 @@
 import React, { useState, useContext } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCogs } from '@fortawesome/free-solid-svg-icons'
+import { faCogs, faTimesCircle } from '@fortawesome/free-solid-svg-icons'
 import { StudySearchContext } from '~/providers/StudySearchProvider'
 
 import OptionsControl from '~/components/search/controls/OptionsControl'
 import useCloseableModal from '~/hooks/closeableModal'
 
+/** list of configured search option entries */
+export const configuredOptions = [
+  { searchProp: 'external', value: 'hca', label: 'Include HCA results' },
+  { searchProp: 'data_types', value: 'raw_counts', label: 'Has raw counts', multiple: true },
+  { searchProp: 'data_types', value: 'diff_exp', label: 'Has differential expression', multiple: true },
+  { searchProp: 'data_types', value: 'spatial', label: 'Has spatial data', multiple: true }
+]
+
+/** Search options button for filtering results by data types/sources */
 export default function OptionsButton() {
   const searchContext = useContext(StudySearchContext)
   const [showOptions, setShowOptions] = useState(false)
-  const configuredOptions = [
-    { searchProp: 'external', value: 'hca', label: 'Include HCA results' },
-    { searchProp: 'data_types', value: 'raw_counts', label: 'Has raw counts', multiple: true },
-    { searchProp: 'data_types', value: 'diff_exp', label: 'Has differential expression', multiple: true },
-    { searchProp: 'data_types', value: 'spatial', label: 'Has spatial data', multiple: true }
-  ]
 
-  const { node, _, handleButtonClick } = useCloseableModal(showOptions, setShowOptions)
+  /** determine if any options have been selected */
+  function searchOptionSelected() {
+    const opts = []
+    configuredOptions.map(option => {
+      const opt = option.searchProp
+      if (searchContext.params[opt] && searchContext.params[opt].length > 0) {
+        opts.push(opt)
+      }
+    })
+    return opts.length > 0
+  }
+
+  /** clear all selected options */
+  function clearAllOptions() {
+    const existingParams = searchContext.params
+    configuredOptions.map(option => {delete existingParams[option.searchProp]})
+    searchContext.updateSearch(existingParams)
+    setShowOptions(false)
+  }
+
+  const { node, clearNode, handleButtonClick } = useCloseableModal(showOptions, setShowOptions)
 
   const optionsMenu = <div data-analytics-name='search-options-menu' id='search-options-menu'>
     <ul>
@@ -33,6 +56,19 @@ export default function OptionsButton() {
         })
       }
     </ul>
+    { showOptions && searchOptionSelected() &&
+      <a className='pull-right' onClick={clearAllOptions}>
+        Clear&nbsp;
+        <span
+          ref={clearNode}
+          data-testid='clear-search-options'
+          onClick={clearAllOptions}
+          aria-label='Clear options'
+        >
+          <FontAwesomeIcon icon={faTimesCircle}/>
+        </span>
+      </a>
+    }
   </div>
 
   return (
@@ -45,7 +81,7 @@ export default function OptionsButton() {
       <a onClick={handleButtonClick}>
         <FontAwesomeIcon className="icon-left" icon={faCogs}/>Options
       </a>
-      { showOptions && optionsMenu}
+      { showOptions && optionsMenu }
     </span>
   )
 }
