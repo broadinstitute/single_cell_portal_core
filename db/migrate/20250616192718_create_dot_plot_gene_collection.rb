863754bb5bb9602e7c893ae675e4f047349c55f2
Eric Weitz <eweitz@broadinstitute.org>
2025-11-06 11:03:59 -0500

    Robustify migrations

--- db/migrate/20250616192718_create_dot_plot_gene_collection.rb
+++ db/migrate/20250616192718_create_dot_plot_gene_collection.rb
@@ -2,7 +2,8 @@ class CreateDotPlotGeneCollection < Mongoid::Migration
   def self.up
     # since these documents will be created by scp-ingest-pipeline, the collection needs to exist first to
     # prevent errors when the job tries to create them
-    DotPlotGene.collection.create
+    # Only create if it doesn't already exist
+    DotPlotGene.collection.create unless DotPlotGene.collection.database.collection_names.include?('dot_plot_genes')
   end
 
   def self.down
--- db/migrate/20251106100123_add_dot_plot_preprocessing_frontend_feature_flag.rb
+++ db/migrate/20251106100123_add_dot_plot_preprocessing_frontend_feature_flag.rb
@@ -1,12 +1,12 @@
-class AddDotPlotPreprocessingFrontendFeatureFlag < ActiveRecord::Migration[6.1]
-  def up
+class AddDotPlotPreprocessingFrontendFeatureFlag < Mongoid::Migration
+  def self.up
     FeatureFlag.find_or_create_by(name: 'dot_plot_preprocessing_frontend') do |flag|
       flag.default_value = false
       flag.description = 'Enable pre-computed dot plot data from backend preprocessing'
     end
   end
 
-  def down
+  def self.down
     flag = FeatureFlag.find_by(name: 'dot_plot_preprocessing_frontend')
     flag.destroy if flag.present?
   end
