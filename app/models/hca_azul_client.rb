f61d0a77c2f6664bb451e99afb76d8219b7c2889
Jonathan Bistline <bistline@broadinstitute.org>
2026-09-17 15:24:49 -0400

    Lowering MAX_RESULTS for Azul to 50

--- app/models/hca_azul_client.rb
+++ app/models/hca_azul_client.rb
@@ -15,7 +15,7 @@ class HcaAzulClient
   MANIFEST_FORMATS = %w[compact full terra.bdbag terra.pfb curl].freeze
 
   # maximum number of results to return
-  MAX_RESULTS = 75
+  MAX_RESULTS = 50
 
   # maximum length of query string (in characters) for requests
   MAX_QUERY_LENGTH = 8192
